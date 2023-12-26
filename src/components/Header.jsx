6b7b32fd28694cd681d26332044908dcf44e903b
ShuntaToda <todashuntawork@gmail.com>
2023-12-26 21:49:58 +0900

    header追加

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav class="navbar bg-body-tertiary">
+      <h1 class="container-fluid">
+        <a href='/' className="navbar-brand">React Translate</a>
+      </h1>
+    </nav>
+  )
+}
+
+export default Header
