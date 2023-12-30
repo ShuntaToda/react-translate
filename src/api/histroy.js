37b4c459c8c45cdcb189478eedc0f5c470893ce8
ShuntaToda <todashuntawork@gmail.com>
2023-12-30 12:44:08 +0900

    url変数定義

--- src/api/histroy.js
+++ src/api/histroy.js
@@ -2,6 +2,6 @@ import axios from "axios"
 import { apiPath } from "./path"
 
 export const getHistory = async (limit) => {
-  const res = await axios.get("https://q0uaty8gwa.execute-api.ap-northeast-1.amazonaws.com/dev/translate", { params: { limit: limit } })
+  const res = await axios.get(apiPath, { params: { limit: limit } })
   return res.data
 }
